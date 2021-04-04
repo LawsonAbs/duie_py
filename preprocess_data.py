dfeb404d35385041906485b443b510153e97cde3
LawsonAbs <shenliu@ahnu.edu.cn>
2021-04-05 06:14:50 +0800

    数据预处理

---
+++ preprocess_data.py
@@ -0,0 +1,41 @@
+import json
+
+"""
+预处理数据，得到 subject2id.json, object2id.json 文件
+"""
+def get_subject2id(in_path,out_path):
+    subject_map={}        
+    with open(in_path,'r') as f:
+        cont = json.load(f)
+        index = 2
+        for row in cont:
+            if row['subject_type'] not in subject_map.keys():
+                subject_map[row['subject_type']] = index
+                index+=1
+    
+    with open(out_path,'w',encoding="utf-8") as f:
+        json.dump(subject_map,f,ensure_ascii=False)
+
+
+
+"""
+预处理数据，得到 object2id.json 文件
+"""
+def get_object2id(in_path,out_path):
+    object_map={}        
+    with open(in_path,'r') as f:
+        cont = json.load(f)
+        index = 2
+        for row in cont:
+            vals = list(row['object_type'].values())            
+            for val in vals:
+                if val not in object_map.keys():
+                    object_map[val] = index
+                    index+=1
+    with open(out_path,'w',encoding="utf-8") as f:
+        json.dump(object_map,f,ensure_ascii=False)
+
+if __name__ == "__main__":
+    in_path = "./data/duie_schema.json"
+    out_path = "./data/subject2id.json"
+    get_subject2id(in_path,out_path)
