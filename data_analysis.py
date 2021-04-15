cde63de74baa89d3e05e6570c1267d6d6fb9018e
LawsonAbs <shenliu@ahnu.edu.cn>
2021-04-15 23:11:33 +0800

    添加分析test_data_predict中错误数据的分析

--- data_analysis.py
+++ data_analysis.py
@@ -25,7 +25,64 @@ def get_subject_class_num(train_data_path):
     
     print(subject_class_num)
 
+def look_error():
+    data_path = "/home/lawson/program/DuIE_py/data/test_data_predict.json"
+    valid = []
+    out_path = "/home/lawson/program/DuIE_py/data/test_data_predict_valid.json"
+    cnt = 0
+    with open(data_path,'r') as f:
+        line = f.readline()        
+        while(line):
+            line = json.loads(line) # 变成一个json数据
+            #print(line)
+            spo_list = line['spo_list']
+            valid_spo = []
+            for spo in spo_list:
+                object = spo['object']                
+                #print(object)
+                if '@value' not in object.keys():
+                    cnt +=1
+                    #print(line['text'])
+                    break
+                else:
+                    valid_spo.append(spo)
+            cur = {}
+            cur['text'] = line['text']
+            cur['spo_list'] = valid_spo
+            valid.append(cur)
+            line = f.readline()
+    print(cnt)
+
+    with open(out_path,'w') as f:
+        for line in valid:
+            json.dump(line,f,ensure_ascii=False)
+            f.write("\n")
+            # f.(str(line)+"\n")
+    
+
+def look_error_2():
+    data_path = "/home/lawson/program/DuIE_py/data/test_data_predict.json"
+    valid = []
+    out_path = "/home/lawson/program/DuIE_py/data/test_data_predict_valid.json"
+    cnt = 0
+    with open(data_path,'r') as f:
+        line = f.readline()        
+        while(line):
+            line = line.strip("\n")
+            #line = "{" + line
+            line += "}"
+            line = json.loads(line) # 变成一个json数据            
+            valid.append(line)
+            line = f.readline()
+
+    with open(out_path,'w') as f:
+        for line in valid:
+            json.dump(line,f,ensure_ascii=False)
+            f.write("\n")
+            # f.(str(line)+"\n")
+
 if __name__ == "__main__":
-    get_subject_class_num(
-        train_data_path='./data/dev_data.json'
-    )
+    # get_subject_class_num(
+    #     train_data_path='./data/dev_data.json'
+    # )
+    look_error()
