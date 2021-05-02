723fcfd6fcb5e0c9cf406e7c0815aeedb466dc29
LawsonAbs <shenliu@ahnu.edu.cn>
2021-05-02 16:14:56 +0800

    在最后的分类模型中显式使用position_embedding

--- models.py
+++ models.py
@@ -6,8 +6,10 @@ FilePath: /duie_py/models.py
 '''
 import torch as t
 import torch.nn as nn
+from torch.nn.parameter import Parameter
 from transformers import BertConfig,BertModel,BertForSequenceClassification
 from torchcrf import CRF
+from transformers.utils.dummy_tokenizers_objects import BertTokenizerFast
 
 class SubjectModel(nn.Module):
     def __init__(self,name_or_path,in_fea,out_fea):
@@ -22,7 +24,7 @@ class SubjectModel(nn.Module):
                          )
         last_layer = out.last_hidden_state
         # size = [batch_size,max_seq_length,768]
-        out = self.linear(last_layer)        
+        out = self.linear(last_layer)
         return out
         # size = [batch_size,max_seq_length,out_fea]
 
@@ -46,20 +48,41 @@ class ObjectModel(nn.Module):
         out = self.linear(last_layer)                   
         return out
 
+
+
 class RelationModel(nn.Module):
     def __init__(self,name_or_path,relation_class_num):
         super(RelationModel,self).__init__()     
         # 这里重新配置了一个config，是为了使用 BertForSequenceClassfication 进行文本分类
         self.config = BertConfig.from_pretrained(name_or_path,num_labels = relation_class_num)
-        
-        # 使用 BertForSequenceClassification 的坏处在于，如果最后想要用模型预测该怎么办？
+                
         # 可以不用传label，直接对应输出 logits
-        self.model = BertForSequenceClassification.from_pretrained(name_or_path, config=self.config)        
+        #self.model = BertForSequenceClassification.from_pretrained(name_or_path, config=self.config)
+        self.model = BertModel.from_pretrained(name_or_path, config=self.config)
+        #self.my_position_embedding = t.randn((256,768),requires_grad=True,device='cuda') # 随机初始化一个位置向量，用于训练前后的关系
+        self.dropout = nn.Dropout(0.1)
+        self.classifier = nn.Linear(768,relation_class_num)
+        
+        self.my_position_embedding =  nn.Embedding(256,768)
+        self.tokenizer = BertTokenizerFast.from_pretrained(name_or_path)
+        # self.register_parameter(name='my_position_embedding',param=Parameter(self.my_position_embedding))
 
-    def forward(self,input_ids,token_type_ids,attention_mask,labels):
-        out = self.model(input_ids=input_ids,
+    def forward(self,input_ids,token_type_ids,attention_mask,labels,indexs):
+        outputs = self.model(input_ids=input_ids,
                          token_type_ids=token_type_ids,
-                         attention_mask=attention_mask,
-                         labels = labels
+                         attention_mask=attention_mask,    
+                         #labels = labels
+                         #output_hidden_states = True
                          )
-        return out
+        #cls_emb = out.last_hidden_state[:,0,:]        
+        #cls_emb = out.hidden_states[12][:,0,:]
+        
+        
+        cur_position_emb = self.my_position_embedding(indexs)            
+        temp = outputs[1]
+        pooled_output = temp + cur_position_emb
+        logits = self.classifier(pooled_output)
+        return logits
+        
+    
+        
